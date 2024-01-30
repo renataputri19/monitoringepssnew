a45c8df82bd9853b531bb4ccc913a428bf76b411
renataputri19 <renataputri19@gmail.com>
2024-01-30 08:11:38 +0700

    30 jan

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->boolean('admin')->default(false); // Add a boolean admin field with a default value of false
             $table->rememberToken();
             $table->timestamps();
         });
